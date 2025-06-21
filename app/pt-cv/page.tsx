f783e6e9c02d594e4f58c36f8713ad00478d8f6a
HitasantariaGroup <hitasantariagroup@gmail.com>
2025-06-21 16:44:31 +0800

    Revert "fix: improve text formatting and update pricing details in PT_CVPage"

--- app/pt-cv/page.tsx
+++ app/pt-cv/page.tsx
@@ -55,8 +55,8 @@ export default function PT_CVPage() {
       <HeroSection
         title={
           <>
-            Pendirian CV, PT,& Yayasan{" "}
-            <span className="text-[#0EBCF7]"> Termurah Se Indonesia!</span>{" "}
+            Pendirian CV, PT,& Yayasan <span className="text-[#0EBCF7]"> Termurah
+            Se Indonesia!</span>{" "}
           </>
         }
         description="Dengan harga mulai dari Rp1 jutaan, 
@@ -74,11 +74,10 @@ export default function PT_CVPage() {
       <section className="md:py-16 py-10 ">
         <div className="container mx-auto px-4">
           <h2 className="mb-6 text-center md:text-5xl text-[25px] font-bold">
-            Kenapa Harus Bikin PT/CV di{" "}
-            <span className="text-[#0EBCF7]">Hakiva?</span>{" "}
+              Kenapa Harus Bikin PT/CV di <span className="text-[#0EBCF7]">Hakiva?</span>{" "}
           </h2>
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            <Card className="p-6 shadow-lg">
+          <Card className="p-6 shadow-lg">
               <div className="mb-4 flex h-16 w-16 items-center justify-center rounded-full bg-cyan-100 text-cyan-500">
                 <Image
                   src="/home-1.png"
@@ -89,11 +88,10 @@ export default function PT_CVPage() {
                 />
               </div>
               <h3 className="mb-2 md:text-3xl text-xl font-semibold">
-                Harga Termurah Se Indonesia
+              Harga Termurah Se Indonesia
               </h3>
               <p className="text-[#5B5B5B] md:text-lg text-sm">
-                Legalitas nggak harus mahal. Kami bantu cari paket sesuai
-                kebutuhan dan kondisi kamu.
+              Legalitas nggak harus mahal. Kami bantu cari paket sesuai kebutuhan dan kondisi kamu.
               </p>
             </Card>
             <Card className="p-6 shadow-lg">
@@ -110,9 +108,10 @@ export default function PT_CVPage() {
                 Gratis Konsultasi Tanpa Komitmen
               </h3>
               <p className="text-[#5B5B5B] md:text-lg text-sm">
-                Masih bingung mau pilih CV, PT, atau Yayasan? Tenang, kamu bisa
-                konsultasi dulu GRATIS. Kami bantu analisa kebutuhan usaha kamu
-                dan kasih saran terbaik. Tanpa biaya, tanpa paksaan.
+                Masih bingung mau pilih CV, PT, atau Yayasan? Tenang, 
+                kamu bisa konsultasi dulu GRATIS. Kami bantu analisa 
+                kebutuhan usaha kamu dan kasih saran terbaik. 
+                Tanpa biaya, tanpa paksaan.
               </p>
             </Card>
             <Card className="p-6 shadow-lg">
@@ -129,9 +128,10 @@ export default function PT_CVPage() {
                 Aman & Legal
               </h3>
               <p className="text-[#5B5B5B] md:text-lg text-sm">
-                Masih bingung mau pilih CV, PT, atau Yayasan? Tenang, kamu bisa
-                konsultasi dulu GRATIS. Kami bantu analisa kebutuhan usaha kamu
-                dan kasih saran terbaik. Tanpa biaya, tanpa paksaan..
+                Masih bingung mau pilih CV, PT, atau Yayasan? 
+                Tenang, kamu bisa konsultasi dulu GRATIS. 
+                Kami bantu analisa kebutuhan usaha kamu dan 
+                kasih saran terbaik. Tanpa biaya, tanpa paksaan..
               </p>
             </Card>
             <Card className="p-6 shadow-lg">
@@ -148,8 +148,9 @@ export default function PT_CVPage() {
                 Transparan & Jelas dari Awal
               </h3>
               <p className="text-[#5B5B5B] md:text-lg text-sm">
-                Kamu tahu estimasi waktu & biaya sejak awal. Nggak ada biaya
-                tersembunyi atau proses yang ditutup-tutupi.
+                Kamu tahu estimasi waktu & biaya sejak awal. 
+                Nggak ada biaya tersembunyi 
+                atau proses yang ditutup-tutupi.
               </p>
             </Card>
             <Card className="p-6 shadow-lg">
@@ -166,7 +167,7 @@ export default function PT_CVPage() {
                 Tim Profesional & Ramah
               </h3>
               <p className="text-[#5B5B5B] md:text-lg text-sm">
-                Kita ngerti nggak semua orang familiar dengan istilah hukum.
+                Kita ngerti nggak semua orang familiar dengan istilah hukum. 
                 Makanya kami siap jelaskan dengan bahasa yang gampang dipahami.
               </p>
             </Card>
@@ -184,8 +185,9 @@ export default function PT_CVPage() {
                 Sudah Dipercaya Ratusan Klien
               </h3>
               <p className="text-[#5B5B5B] md:text-lg text-sm">
-                Mulai dari pelaku UMKM, freelancer, sampai startup. Banyak yang
-                sudah merasakan mudahnya ngurus legalitas bareng Hakiva.
+                 Mulai dari pelaku UMKM, freelancer, 
+                 sampai startup. Banyak yang sudah merasakan 
+                 mudahnya ngurus legalitas bareng Hakiva.
               </p>
             </Card>
           </div>
@@ -196,7 +198,8 @@ export default function PT_CVPage() {
       <section className="md:py-16 py-8 my-10">
         <div className="container mx-auto px-4">
           <h2 className="mb-6 text-center md:text-5xl/[1.2] text-2xl font-bold">
-            Layanan <span className="text-[#0EBCF7]">Hakiva</span>{" "}
+            Layanan{" "}
+            <span className="text-[#0EBCF7]">Hakiva</span>{" "}
           </h2>
           <p className="md:text-lg text-sm text-center mb-8 text-[#5B5B5B]">
             Cuma di tempat kami kamu bisa dapet:
@@ -238,6 +241,7 @@ export default function PT_CVPage() {
         </div>
       </section>
 
+
       {/* Testimonials */}
       <TestimonialSection
         title={
@@ -247,6 +251,7 @@ export default function PT_CVPage() {
         }
         testimonials={testimonials}
       />
+
 
       {/* Pricing */}
       <section className="md:py-16 py-10">
@@ -303,17 +308,17 @@ export default function PT_CVPage() {
               </h3>
               <div className="md:mb-10 mb-5 text-center">
                 <span className="text-sm line-through text-semibold md:text-3xl text-base">
-                  IDR 6.500.000
+                  IDR 3.800.000
                 </span>
                 <p className="md:text-5xl text-2xl font-semibold text-[#FFD745]">
-                  IDR 4.000.000
+                  IDR 2.250.000
                 </p>
               </div>
               <p className="md:mb-4 mb-2 md:text-lg text-xs">
                 Lama proses : 2-4 hari kerja ⁽¹⁾
               </p>
               <ol className="md:mb-4 mb-2 list-disc list-inside text-white md:text-base text-[10px]">
-                <li>Pengecekan & Pendaftaran nama PT</li>
+                <li>Pengecekan & Pendaftaran nama CV</li>
                 <li>KBLI Badan Usaha</li>
                 <li>Akta Notaris</li>
                 <li>SK Kemenkumham</li>
@@ -324,7 +329,7 @@ export default function PT_CVPage() {
               <div className="md:mb-10 mb-10 grid grid-cols-2 text-[#47F3FA] grid-cols-[30px_1fr] md:grid-cols-[45px_1fr] md:text-xs text-[8px]/[1.5]">
                 <p>Note :</p>
                 <ol>
-                  <li>⁽¹⁾ Setelah client tanda tangan </li>
+                <li>⁽¹⁾ Setelah client tanda tangan </li>
                 </ol>
               </div>
               <div className="text-center mt-auto">
@@ -347,17 +352,17 @@ export default function PT_CVPage() {
               </h3>
               <div className="md:mb-10 mb-5 text-center">
                 <span className="text-sm line-through text-semibold md:text-3xl text-base">
-                  IDR 7.500.000
+                  IDR 4.500.000
                 </span>
                 <p className="md:text-5xl text-2xl font-semibold text-[#FFD745]">
-                  IDR 4.500.000
+                  IDR 2.500.000
                 </p>
               </div>
               <p className="md:mb-4 mb-2 md:text-lg text-xs">
                 Lama proses : 2-5 hari kerja ⁽¹⁾
               </p>
               <ol className="md:mb-4 mb-2 list-disc list-inside text-white md:text-base text-[10px]">
-                <li>Pengecekan & Pendaftaran nama PT</li>
+                <li>Pengecekan & Pendaftaran nama CV</li>
                 <li>KBLI Badan Usaha</li>
                 <li>Akta Notaris</li>
                 <li>SK Kemenkumham</li>
@@ -370,10 +375,7 @@ export default function PT_CVPage() {
                 <p>Note :</p>
                 <ol>
                   <li>⁽¹⁾ Setelah client tanda tangan </li>
-                  <li>
-                    ⁽²⁾ Logo akan diberikan seminggu setelah penandatanganan
-                    akta notaris
-                  </li>
+                  <li>⁽²⁾ Logo akan diberikan seminggu setelah penandatanganan akta notaris</li>
                 </ol>
               </div>
               <div className="text-center mt-auto">
@@ -397,6 +399,7 @@ export default function PT_CVPage() {
             Pendirian PT (Badan Usaha Mikro)
           </h2>
 
+          
           <div className="grid  lg:grid-cols-2 max-w-[900px] justify-center items-center md:pt-5 grid-cols-[300px] md:grid-cols-[500px] gap-12 mx-auto justify-center ">
             <div className="overflow-hidden  rounded-lg bg-[#000E54] p-8 text-white flex flex-col h-full">
               <h3 className="mb-4 text-center md:text-4xl text-2xl font-semibold">
@@ -425,7 +428,7 @@ export default function PT_CVPage() {
               <div className="md:mb-10 mb-10 grid grid-cols-2 text-[#47F3FA] grid-cols-[30px_1fr] md:grid-cols-[45px_1fr] md:text-xs text-[8px]/[1.5]">
                 <p>Note :</p>
                 <ol>
-                  <li>⁽¹⁾ Setelah client tanda tangan </li>
+                <li>⁽¹⁾ Setelah client tanda tangan </li>
                 </ol>
               </div>
               <div className="text-center mt-auto">
@@ -471,10 +474,7 @@ export default function PT_CVPage() {
                 <p>Note :</p>
                 <ol>
                   <li>⁽¹⁾ Setelah client tanda tangan </li>
-                  <li>
-                    ⁽²⁾ Logo akan diberikan seminggu setelah penandatanganan
-                    akta notaris
-                  </li>
+                  <li>⁽²⁾ Logo akan diberikan seminggu setelah penandatanganan akta notaris</li>
                 </ol>
               </div>
               <div className="text-center mt-auto">
@@ -495,7 +495,7 @@ export default function PT_CVPage() {
         </div>
         <div className="container mx-auto px-4 my-20">
           <h2 className="mb-12 text-center text-3xl font-bold md:text-5xl text-2xl">
-            Paket Pendirian yayasan
+             Paket Pendirian yayasan
           </h2>
           <div className="grid lg:grid-cols-1 max-w-[450px] mx-auto grid-cols-[300px] md:grid-cols-[500px] justify-center ">
             <div className="overflow-hidden rounded-lg bg-[#000E54] md:p-8 p-4 text-white flex flex-col h-full ">
@@ -543,6 +543,7 @@ export default function PT_CVPage() {
           </div>
         </div>
       </section>
+
 
       {/* Footer */}
       <Footer />
